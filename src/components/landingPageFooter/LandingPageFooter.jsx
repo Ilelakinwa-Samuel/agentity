ef443c56c840b8f301bb5fc576f7da362de18baa
mike <aimrmike5@gmail.com>
2026-03-17 20:09:03 +0100

    done

--- src/components/landingPageFooter/LandingPageFooter.jsx
+++ src/components/landingPageFooter/LandingPageFooter.jsx
@@ -2,7 +2,7 @@
 function LandingPageFooter() {
   return (
     <div className="bg-[#050506] text-white p-8 border-t border-[#514c4c] mt-12">
-      <p className="text-center">© 2026 Agentity. Built for Avalanche blockchain. All rights reserved.
+      <p className="text-center">© 2026 Agentity. Built for Hedera blockchain. All rights reserved.
 
 </p>
     </div>
